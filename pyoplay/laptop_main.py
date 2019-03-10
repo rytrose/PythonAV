dcd8d26a4f1b8cf7eab25bf97dcee68cd070e640
Ryan Rose <rytrose@gmail.com>
2019-03-10 17:57:16 +0000

    laptop change

--- pyoplay/laptop_main.py
+++ pyoplay/laptop_main.py
@@ -1,5 +1,5 @@
 from websocket import WS
 
 if __name__ == '__main__':
-    ws_client = WS(uri="ws://rytrose-pi-zero-w.local:8000")
+    ws_client = WS(is_client=True, uri="ws://rytrose-pi-zero-w.local:8000")
     ws_client.start()
