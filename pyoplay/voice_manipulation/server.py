5c4e1e0f25c3d428fcd0927cfa9f3cd3a1d0a5d1
Ryan Rose <rytrose@gmail.com>
2019-07-14 17:50:19 -0400

    playback on server

--- pyoplay/voice_manipulation/server.py
+++ pyoplay/voice_manipulation/server.py
@@ -3,22 +3,27 @@ import sys
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
 
 import pickle
+from pyo import *
 from communication.osc_client import OSCClient
 from pyo_extensions.pyo_client import PyoClient
 
-c = PyoClient(default_audio_device="built-in")
 
-osc_client = OSCClient(local_address="Ryans-MacBook-Pro.local", local_port=5001, 
-                       remote_address="rytrose-pi-zero-w.local", remote_port=5000)
+class VoiceManipulationServer:
+    def __init__(self):        
+        self.osc_client = OSCClient(local_address="Ryans-MacBook-Pro.local", local_port=5001, 
+                            remote_address="rytrose-pi-zero-w.local", remote_port=5000)
+        self.osc_client.map("/segment", self.on_segment)
+        self.osc_client.begin()
 
-def on_segment(address, segment_bytes):
-    segment = pickle.loads(segment_bytes)
-    print("Server got segment:", segment)
-    osc_client.send("/segment", segment_bytes)
+    def on_segment(self, address, segment_bytes):
+        self.segment = pickle.loads(segment_bytes)
+        print("Server got segment:", self.segment)
+        self.osc_client.send("/segment", segment_bytes)
 
-    ls = Linseg(self.segment, loop=True)
-    saw = SuperSaw(freq=self.ls).out()
-    ls.play()
+        self.ls = Linseg(self.segment, loop=True)
+        self.saw = SuperSaw(freq=self.ls).mix(2).out()
+        self.ls.play()
 
-osc_client.map("/segment", on_segment)
-osc_client.begin()
+if __name__ == "__main__":
+    c = PyoClient(default_audio_device="built-in")
+    v = VoiceManipulationServer()
